4f0b9e249174373038308d38f04427e69cb3de4f
mincheolkk <kmc04080408@gmail.com>
2021-07-02 16:49:11 +0900

    ADD : 삭제 추가

--- src/main/java/org/example/springboot/service/PostsService.java
+++ src/main/java/org/example/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package org.example.springboot.service;
 
 import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor  // final 이 선언된 필드, @NonNull 제약 조건이 있는 필드들을 인자값으로 하는 생성자를 대신 생성해줌
 @Service
@@ -18,7 +22,7 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).
-                                                    getId();
+                getId();
     }
 
     @Transactional
@@ -32,11 +36,26 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new
                         IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지하고 조회만 가능하게 만들어서 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)  // map(posts -> new PostsListResponseDto(posts)) 와 같음
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        postsRepository.delete(posts);
     }
 }
